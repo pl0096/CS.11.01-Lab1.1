9b0d49887eb1b3fdeb3db42b4406c579955c1c9e
perlielui <perliel2026@student.cis.edu.hk>
2023-09-07 14:51:39 +0800

    Added Dateable and Pages code

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = -1;
+        while (age < 0){
+            try{
+                age = Integer.parseInt(scanner.nextLine());
+
+            }
+            catch(NumberFormatException e){
+                System.out.println("Please enter a valid number!");
+            }
+        }
+
+        int lover = 7 + age / 2;
+        String ageString = "" + age;
+        String loverString = "" + lover;
+        System.out.println(ageString + "-year olds should date somebody who is at least " + loverString + " years old. ");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number!");
+            }
+        }
 
+
+        int pages = 100 - age;
+        String ageString = "" + age;
+        String pagesString = "" + pages;
+        System.out.println(ageString +"-year olds should read at least " + pagesString + " pages before giving up on a book.");
     }
 }
